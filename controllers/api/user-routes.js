5a8f5e343405ef17356edc34c4c1ba04c5bd184a
Megan McLean <425megs@gmail.com>
2022-02-23 18:21:38 -0800

    user routes finit

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,11 @@
 const { User } = require("../models");
 
+// GET all users
+
+// GET user by __id, also populate thought and friend data
+
+// POST new user
+
 module.exports = {
     getAllUser(req, res){
         User.find()
@@ -7,7 +13,7 @@ module.exports = {
         .catch((err) => res.status(500).json(err))
     },
     getSingleUser(req,res){
-        User.findOne({_id: req.params.id})
+        User.findOne({_id: req.params.userId})
             .populate({
                 path: 'friends',
                 select: '-__v'
@@ -37,8 +43,73 @@ module.exports = {
     },
 }
 
-// GET all users
+// Update existing user
+editUser(req,res){
+    User.findOneAndUpdate(
+        {_id: req.params.userId},
+        req.body,
+        {runValidator: true, new:true}
+    )
+    .then(editUser => {
+        if(!editUser){
+            res.status(404).json({message: 'no user with that id found'})
+            return
+        }
+        res.status(200).json(editUser)
+    })
+    .catch(err => {
+        res.status(500).json(err)
+    }),
 
-// GET user by __id, also populate thought and friend data
+    deleteUser(req, res){
+        User.findOneAndDelete(
+            {_id: req.params.userId}
+        )
+        .then(deleteUser => {
+            if (!deleteUser){
+                res.status(404).json({message: 'user dont exist'})
+                return
+            }
+            res.status(200).json(deleteUser)
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+    },
+    addFriend(req, res){
+        User.findOneAndUpdate(
+            {_id: req.params.userId},
+            { $push: {friends: req.params.friendId}},
+            {runValidators: true, new:true}
+        )
+        .then(newFriend => {
+            if (!newFriend){
+                res.status(404).json({message: 'no friends for you'})
+                return
+            }
+            res.status(200).json(newFriend)
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+    },
 
-// POST new user
+    deleteFriend(req,res){
+        User.findOneAndUpdate(
+        {_id: req.params.userId},
+        { $push: {friends: req.params.friendId}},
+        {new:true}
+    )
+    .then(byeFriend => {
+        if (!byeFriend){
+            res.status(404).json({message: 'no friends for you'})
+            return
+        }
+        res.status(200).json(byeFriend)
+    })
+    .catch(err => {
+        res.status(500).json(err)
+    })
+},
+
+}
